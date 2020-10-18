929c84a4bc07637af60abdd57e9cac2ed2374881
veronikadumalo <dumalo44084@gmail.com>
2020-10-18 18:15:35 +0200

    section menu change

---
+++ portfolio.js
@@ -0,0 +1,39 @@
+﻿//fuction open or cloth more information about projects
+function MoreProject() {
+    var moreDots = document.getElementById("moreDots");
+    var moreText = document.getElementById("moreText");
+    var moreButton = document.getElementById("moreButton");
+   
+
+    if (moreDots.style.display === "none") {
+        moreDots.style.display = "inline";
+        moreButton.innerHTML = "WIĘCEJ";
+        moreText.style.display = "none";
+    } else {
+        moreDots.style.display = "none";
+        moreButton.innerHTML = "MNIEJ";
+        moreText.style.display = "inline";
+    }
+}
+
+jQuery(document).ready(function ($) {
+    var sections = $('section')
+        , nav = $('nav')
+        , nav_height = nav.outerHeight()+10;
+    $(window).on('scroll', function () {
+        var position = $(this).scrollTop();
+        sections.each(function () {
+            var topSection = $(this).offset().top - nav_height,
+                bottomSection = topSection + $(this).outerHeight() ;
+            if (position >= topSection && position <= bottomSection) {
+                nav.find('a').removeClass('active');
+                sections.removeClass('active');
+                $(this).addClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            }
+        });
+    });
+});
+
+
+
