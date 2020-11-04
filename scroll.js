81ffe582ff8f8ff1f772a14b4ad1882b884ca388
veronikadumalo <dumalo44084@gmail.com>
2020-11-04 22:07:29 +0100

    scrollreveal in diferent file

---
+++ scroll.js
@@ -0,0 +1,10 @@
+window.scroll = new ScrollReveal();
+scroll.reveal('.project_box', {
+    opacity: 0,
+    interval: 500
+});
+scroll.reveal('.summary_box', {
+    delay: 250,
+    origin: "left",
+    distance: "50px"
+});
